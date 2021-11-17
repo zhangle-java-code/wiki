d49b74140bc556a61db55a8ba7a21f5dc5335680
zhangle <zhangle@vtstar.net>
2021-11-18 06:23:20 +0800

    fix(application.properties): 配置项文件获取使用

--- src/main/java/com/zh/wiki/controller/TestController.java
+++ src/main/java/com/zh/wiki/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.zh.wiki.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,10 @@ import java.util.Map;
 @RestController
 public class TestController {
 
+    //! 获取默认值
+    @Value("${test.hello:Test}")
+    private String testHello;
+
     //! 支持所有请求方式`
     //@RequestMapping("/hello")
     @GetMapping("/hello")
@@ -20,7 +25,7 @@ public class TestController {
     //@PutMapping("/hello")
     //@DeleteMapping("/hello")
     public String hello(){
-        return "hello world1";
+        return "hello world1," + testHello;
     }
 
 
