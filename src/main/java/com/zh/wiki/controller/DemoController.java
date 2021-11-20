83b13e080dd6854660950fc93d7093ad2385d011
zhangle <zhangle@vtstar.net>
2021-11-20 22:06:21 +0800

    mybattis generator

--- src/main/java/com/zh/wiki/controller/DemoController.java
+++ src/main/java/com/zh/wiki/controller/DemoController.java
@@ -3,19 +3,21 @@ package com.zh.wiki.controller;
 import com.zh.wiki.domin.Demo;
 import com.zh.wiki.service.DemoService;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
 
 @RestController
+@RequestMapping("/demo")
 public class DemoController {
 
     @Resource
     private DemoService demoService;
 
 
-    @PostMapping("/demo/list")
+    @PostMapping("/list")
     public List<Demo> list(){
         return demoService.list();
     }
