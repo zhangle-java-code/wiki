60ec676b6e5c7dc7042f5b14feb552cf901e4ecc
zhangle <zhangle@vtstar.net>
2021-11-20 21:15:40 +0800

    mybatis实现

--- doc/all.sql
+++ doc/all.sql
@@ -1,9 +1,9 @@
 drop table if exists `test`;
 create table `test` (
-                        `id` bigint not null COMMENT 'id',
-                        `name` varchar(50) COMMENT '名称',
-                        `password` varchar(50) COMMENT '密码',
-                        primary key (`id`)
-) engine=InnoDB default charset=utf8mb4 comment= '测试'
+    `id` bigint not null COMMENT 'id',
+    `name` varchar(50) COMMENT '名称',
+    `password` varchar(50) COMMENT '密码',
+    primary key (`id`)
+) engine=InnoDB default charset=utf8mb4 comment= '测试';
 
-
+insert into `test` (id, name, password) values (1,'测试','password')
