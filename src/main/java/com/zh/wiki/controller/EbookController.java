9b69c83922df99e42ce1bf7687135f1c269dba16
zhangle <zhangle@vtstar.net>
2021-11-21 18:05:15 +0800

    封装返回结果

--- src/main/java/com/zh/wiki/controller/EbookController.java
+++ src/main/java/com/zh/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.zh.wiki.controller;
 
 import com.zh.wiki.domain.Ebook;
+import com.zh.wiki.resp.CommonResp;
 import com.zh.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,10 @@ public class EbookController {
 
 
     @PostMapping("/list")
-    public List<Ebook> list() {
-        return ebookService.list();
+    public CommonResp list() {
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
