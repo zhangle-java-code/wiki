b67f025f9ded87b03b784b193c4bdb83599fd594
zhangle <zhangle@vtstar.net>
2021-11-21 21:38:28 +0800

    封装对象赋值对象，封装包装对象

--- src/main/java/com/zh/wiki/service/EbookService.java
+++ src/main/java/com/zh/wiki/service/EbookService.java
@@ -6,6 +6,7 @@ import com.zh.wiki.mapper.EbookMapper;
 import com.zh.wiki.req.EbookReq;
 import com.zh.wiki.resp.CommonResp;
 import com.zh.wiki.resp.EbookResp;
+import com.zh.wiki.util.CopyUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -25,16 +26,22 @@ public class EbookService {
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         criteria.andNameLike("%" + req.getName() + "%");
-        List<Ebook> ebooksList = ebookMapper.selectByExample(ebookExample);
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebooksList) {
-            EbookResp ebookResp = new EbookResp();
-            //ebookResp.setId(ebook.getId());
-            //! 拷贝属性,讲一个对象属性copy另一个属性
-            BeanUtils.copyProperties(ebook,ebookResp);
-            respList.add(ebookResp);
-        }
-        return respList;
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+        //List<EbookResp> respList = new ArrayList<>();
+ //       for (Ebook ebook : ebooksList) {
+ ///*           EbookResp ebookResp = new EbookResp();
+ //           //ebookResp.setId(ebook.getId());
+ //           //! 拷贝属性,讲一个对象属性copy另一个属性
+ //           BeanUtils.copyProperties(ebook,ebookResp);*/
+ //           // 功能函数复制
+ //           //EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+ //           respList.add(ebookResp);
+ //       }
+ //       return respList;
+
+        // ! 循环，进行属性赋值
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+        return list;
     }
 
 }
