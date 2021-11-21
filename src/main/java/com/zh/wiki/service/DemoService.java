9bf2105d9eb0f3af408d7792691c3b3f1667bb7a
zhangle <zhangle@vtstar.net>
2021-11-21 10:20:20 +0800

    集成mybatis generator生成mapper表

---
+++ src/main/java/com/zh/wiki/service/DemoService.java
@@ -0,0 +1,21 @@
+package com.zh.wiki.service;
+
+import com.zh.wiki.domain.Demo;
+import com.zh.wiki.mapper.DemoMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class DemoService {
+
+    @Resource
+    private DemoMapper demoMapper;
+
+
+    public List<Demo> list(){
+        return  demoMapper.selectByExample(null);
+    }
+
+}
