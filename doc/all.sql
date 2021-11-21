887a4713e26ec077056dfdb64ef6a9061f44cf31
zhangle <zhangle@vtstar.net>
2021-11-21 09:54:04 +0800

    集成mybatis generator生成mapper表

--- doc/all.sql
+++ doc/all.sql
@@ -12,10 +12,11 @@ insert into `test` (id, name, password) values (1,'测试','password');
 select * from `test`;
 
 
+# Demo表
 drop table if exists `demo`;
 create table `demo` (
                         `id` bigint not null COMMENT 'id',
                         `name` varchar(50) COMMENT '名称',
-                        `password` varchar(50) COMMENT '密码',
                         primary key (`id`)
 ) engine=InnoDB default charset=utf8mb4 comment= '测试';
+insert into `demo` (id, name) values (1,'测试');
