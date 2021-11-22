423717fb9aab6cf8c9c35f962d3d5eb578612bc0
zhangle <zhangle@vtstar.net>
2021-11-22 22:12:52 +0800

    添加切面aspect

--- src/main/java/com/zh/wiki/service/EbookService.java
+++ src/main/java/com/zh/wiki/service/EbookService.java
@@ -1,5 +1,7 @@
 package com.zh.wiki.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.zh.wiki.domain.Ebook;
 import com.zh.wiki.domain.EbookExample;
 import com.zh.wiki.mapper.EbookMapper;
@@ -7,6 +9,8 @@ import com.zh.wiki.req.EbookReq;
 import com.zh.wiki.resp.CommonResp;
 import com.zh.wiki.resp.EbookResp;
 import com.zh.wiki.util.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +21,8 @@ import java.util.List;
 @Service
 public class EbookService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
+
     @Resource
     private EbookMapper ebookMapper;
 
@@ -26,7 +32,14 @@ public class EbookService {
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         criteria.andNameLike("%" + req.getName() + "%");
+        // ! 查询条件固定写法
+        PageHelper.startPage(1,3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+        //! 查询总页数
+        PageInfo<Ebook> pageInfo = new PageInfo<>();
+        LOG.warn("总行数: {}", pageInfo.getTotal());
+        LOG.info("总行数: {}", pageInfo.getPages());
+
         //List<EbookResp> respList = new ArrayList<>();
  //       for (Ebook ebook : ebooksList) {
  ///*           EbookResp ebookResp = new EbookResp();
