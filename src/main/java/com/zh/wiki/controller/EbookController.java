362982000f5d6ed15eb75b196faef14dd7a19d58
zhangle <zhangle@vtstar.net>
2021-11-27 20:23:21 +0800

    异常拦截

--- src/main/java/com/zh/wiki/controller/EbookController.java
+++ src/main/java/com/zh/wiki/controller/EbookController.java
@@ -9,6 +9,7 @@ import com.zh.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/ebook")
@@ -18,7 +19,7 @@ public class EbookController {
 
 
     @GetMapping("/list")
-    public CommonResp list(EbookQueryReq req) {
+    public CommonResp list(@Valid EbookQueryReq req) {
         CommonResp<PageResp<EbookQueryResp>> resp = new CommonResp<>();
         PageResp<EbookQueryResp> list = ebookService.list(req);
         resp.setContent(list);
