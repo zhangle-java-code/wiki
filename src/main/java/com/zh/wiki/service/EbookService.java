22511c012b9c5344ce98a1a4eb18a0e89bd56c3f
zhangle <zhangle@vtstar.net>
2021-11-27 09:54:50 +0800

    新增电子书

--- src/main/java/com/zh/wiki/service/EbookService.java
+++ src/main/java/com/zh/wiki/service/EbookService.java
@@ -10,6 +10,7 @@ import com.zh.wiki.req.EbookSaveReq;
 import com.zh.wiki.resp.EbookQueryResp;
 import com.zh.wiki.resp.PageResp;
 import com.zh.wiki.util.CopyUtil;
+import com.zh.wiki.util.SnowFlake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -25,6 +26,9 @@ public class EbookService {
 
     @Resource
     private EbookMapper ebookMapper;
+
+    @Resource
+    private SnowFlake snowFlake;
 
 
     public PageResp<EbookQueryResp> list(EbookQueryReq req){
@@ -68,6 +72,7 @@ public class EbookService {
         Ebook ebook = CopyUtil.copy(req, Ebook.class);
         if (ObjectUtils.isEmpty(req.getId())){
             // 新增
+            ebook.setId(snowFlake.nextId());
             ebookMapper.insert(ebook);
         }else {
             // 修改
