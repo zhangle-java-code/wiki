a37e02219cafd74645e40090f166d3889e8313f8
zhangle <zhangle@vtstar.net>
2021-11-27 20:46:35 +0800

    分类实现

---
+++ src/main/java/com/zh/wiki/controller/CategoryController.java
@@ -0,0 +1,11 @@
+package com.zh.wiki.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+
+}
