ff1bde476505619f81501b3051ed3612976492ac
zhangle <zhangle@vtstar.net>
2021-11-27 10:00:28 +0800

    新增电子书

--- src/main/java/com/zh/wiki/service/EbookService.java
+++ src/main/java/com/zh/wiki/service/EbookService.java
@@ -79,5 +79,9 @@ public class EbookService {
             ebookMapper.updateByPrimaryKey(ebook);
         }
     }
+    //
+    public void delete(Long id){
+        ebookMapper.deleteByPrimaryKey(id);
+    }
 
 }
