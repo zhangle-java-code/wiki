0bd2f5e8b38f66ebfaf9e1a249e52bd021bee184
zhangle <zhangle@vtstar.net>
2022-04-24 20:46:55 +0800

    增加测试过滤器

--- src/main/java/com/zh/wiki/filter/LogFilter.java
+++ src/main/java/com/zh/wiki/filter/LogFilter.java
@@ -1,34 +1,34 @@
 package com.zh.wiki.filter;
 
- import org.slf4j.Logger;
- import org.slf4j.LoggerFactory;
- import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+//? 过滤器是servlet的概念，过滤器是给netty或tomcat
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+//! 过滤器实现
+//! 增加过滤器注解，springBoot扫描，容器就会拿到过滤器
+@Component
+public class LogFilter implements Filter {
 
- import javax.servlet.*;
- import javax.servlet.http.HttpServletRequest;
- import java.io.IOException;
+    private static final Logger LOG = LoggerFactory.getLogger(LogFilter.class);
 
- @Component
- public class LogFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
 
-     private static final Logger LOG = LoggerFactory.getLogger(LogFilter.class);
+    }
 
-     @Override
-     public void init(FilterConfig filterConfig) throws ServletException {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        // 打印请求信息
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        LOG.info("------------- LogFilter 开始 -------------");
+        LOG.info("请求地址: {} {}", request.getRequestURL().toString(), request.getMethod());
+        LOG.info("远程地址: {}", request.getRemoteAddr());
 
-     }
-
-     @Override
-     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-         // 打印请求信息
-         HttpServletRequest request = (HttpServletRequest) servletRequest;
-         LOG.info("------------- LogFilter 开始 -------------");
-         LOG.info("请求地址: {} {}", request.getRequestURL().toString(), request.getMethod());
-         LOG.info("远程地址: {}", request.getRemoteAddr());
-
-         // !时间
-         long startTime = System.currentTimeMillis();
-         filterChain.doFilter(servletRequest, servletResponse);
-         LOG.info("------------- LogFilter 结束 耗时：{} ms -------------", System.currentTimeMillis() - startTime);
-     }
- }
+        long startTime = System.currentTimeMillis();
+        filterChain.doFilter(servletRequest, servletResponse);
+        LOG.info("------------- LogFilter 结束 耗时：{} ms -------------", System.currentTimeMillis() - startTime);
+    }
+}
