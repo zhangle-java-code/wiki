0dc09baf748f70a75a6906206f23080b3eea1475
zhangle <zhangle@vtstar.net>
2022-07-15 23:14:07 +0800

    异常处理

--- doc/常用.sql
+++ doc/常用.sql
@@ -1,5 +1,9 @@
-# 统计单个电子书
-select count(1), SUM(view_count) from doc where ebook_id = 2;
+# 统计单个电子书, 一对多
+select
+    name,
+    count(1) as count_total,
+    SUM(view_count) as view_count_total
+from doc where ebook_id = 2;
 
 
 select
@@ -13,19 +17,49 @@ from
 group by ebook_id;
 
 # 两个表相互关联
-update ebook t1,(select ebook_id,name,count(1) doc_count, SUM(view_count) view_count ,SUM(vote_count) vote_count from doc group by ebook_id) t2
-set t1.doc_count = t2.doc_count,t1.view_count=t2.view_count,t1.vote_count=t2.vote_count
-where t1.id = t2.ebook_id;
+update
+    ebook t1,
+    (select ebook_id,
+            name,
+            count(1) doc_count,
+            SUM(view_count) view_count ,
+            SUM(vote_count) vote_count
+     from doc
+     group by ebook_id
+     )
+        t2
+set t1.doc_count = t2.doc_count,
+    t1.view_count=t2.view_count,
+    t1.vote_count=t2.vote_count
+where
+    t1.id = t2.ebook_id;
 
 
 
-select id, curdate(), 0, 0, 0, 0 from ebook;
+select
+    id,
+    curdate(),
+    0,
+    0,
+    0,
+    0
+from ebook;
 
 
-insert into ebook_snapshot(ebook_id, `date`, view_count, vote_count, view_increase, vote_increase)
-select t1.id, curdate(), 0, 0, 0, 0
+insert into
+    ebook_snapshot(ebook_id, `date`, view_count, vote_count, view_increase, vote_increase)
+select
+    t1.id,
+    curdate(),
+    0,
+    0,
+    0,
+    0
 from ebook t1
-where not exists(select 1
-                 from ebook_snapshot t2
-                 where t1.id = t2.ebook_id
-                   and t2.`date` = curdate());
+where not exists(
+    select 1
+    from ebook_snapshot t2
+    where t1.id = t2.ebook_id
+      and t2.`date` = curdate()
+    )
+;
